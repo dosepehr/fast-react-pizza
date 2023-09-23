8ed6487e431fb24f6bdd69ac6f2cde27cdc36e4e
dosepehr <dosepehr@gmail.com>
2023-09-24 01:09:14 +0330

    add apiSlice reducer to the store

--- src/redux/store/index.js
+++ src/redux/store/index.js
@@ -1,5 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
+import { apiSlice } from '../reducers/apiSlice';
 
 export const store = configureStore({
-    reducer: {},
+    reducer: {
+        [apiSlice.reducerPath]: apiSlice.reducer,
+    },
+    middleware: (getDefaultMiddleware) =>
+        getDefaultMiddleware().concat(apiSlice.middleware),
 });
+
