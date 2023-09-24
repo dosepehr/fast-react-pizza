9db62edff286372fd8c5d028ba601266302a56b2
dosepehr <dosepehr@gmail.com>
2023-09-24 19:24:09 +0330

    add addToCart reducer

--- src/components/MenuItem.jsx
+++ src/components/MenuItem.jsx
@@ -1,8 +1,13 @@
 import React from "react";
 import { formatCurrency } from "../helpers/formatCurrency";
-
+import { useDispatch } from "react-redux";
+import { addToCart } from "../redux/reducers/cartSlice";
 const MenuItem = ({ pizza }) => {
   const { id, name, unitPrice, ingredients, soldOut, imageUrl } = pizza;
+  const dispatch = useDispatch();
+  const handleAddToCart = () => {
+    dispatch(addToCart({ ...pizza, qty: 1 }));
+  };
   return (
     <li className="flex gap-x-4 py-2">
       <div>
@@ -28,7 +33,10 @@ const MenuItem = ({ pizza }) => {
             )}
           </div>
           {!soldOut && (
-            <button className="rounded-full bg-yellow-400 px-4 py-2 text-xs font-semibold uppercase tracking-wide text-stone-800 outline-none duration-300 hover:bg-yellow-300 focus:bg-yellow-300 focus:ring focus:ring-yellow-300 focus:ring-offset-2">
+            <button
+              onClick={handleAddToCart}
+              className="rounded-full bg-yellow-400 px-4 py-2 text-xs font-semibold uppercase tracking-wide text-stone-800 outline-none duration-300 hover:bg-yellow-300 focus:bg-yellow-300 focus:ring focus:ring-yellow-300 focus:ring-offset-2"
+            >
               add to cart
             </button>
           )}
--- src/redux/reducers/cartSlice.js
+++ src/redux/reducers/cartSlice.js
@@ -7,7 +7,19 @@ const cartSlice = createSlice({
     cartTotalQty: 0,
     cartTotalAmount: 0,
   },
-  reducers: {},
+  reducers: {
+    addToCart(state, action) {
+      const selectedPizzaIndex = state.cartItems.findIndex(
+        (item) => item.id === action.payload.id,
+      );
+      if (selectedPizzaIndex < 0) {
+        state.cartItems.push(action.payload);
+      }
+      localStorage.setItem("cartItems", JSON.stringify(state.cartItems));
+    },
+  },
 });
+
+export const { addToCart } = cartSlice.actions;
 
 export default cartSlice.reducer;
