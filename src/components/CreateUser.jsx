81ee8fc9a7e3ca363bdbc9acd4fa2d4200c66cc1
dosepehr <dosepehr@gmail.com>
2023-09-24 17:10:47 +0330

    receive username

--- src/components/CreateUser.jsx
+++ src/components/CreateUser.jsx
@@ -1,4 +1,13 @@
+import { useState } from "react";
+import { useDispatch } from "react-redux";
+import { addUsername } from "../redux/reducers/userSlice";
+
 const CreateUser = () => {
+  const [name, setName] = useState("");
+  const dispatch = useDispatch();
+  const handleUserName = () => {
+    dispatch(addUsername(name));
+  };
   return (
     <div className="flex flex-col items-center">
       <p className=" text-sm text-stone-600 md:text-base">
@@ -6,13 +15,20 @@ const CreateUser = () => {
       </p>
 
       <input
+        value={name}
+        onChange={(e) => setName(e.target.value)}
         type="text"
         placeholder="Your full name"
         className="input my-6 w-72 rounded-full px-4 py-3 text-sm outline-none ring ring-stone-200 duration-300 focus:ring-yellow-500"
       />
-      <button className="rounded-full bg-yellow-400 px-3 py-3 text-sm font-semibold tracking-wide text-stone-800 outline-none duration-300 hover:bg-yellow-300 focus:bg-yellow-300 focus:ring focus:ring-yellow-300 focus:ring-offset-2">
-        START ORDERING
-      </button>
+      {name && (
+        <button
+          className="rounded-full bg-yellow-400 px-3 py-3 text-sm font-semibold tracking-wide text-stone-800 outline-none duration-300 hover:bg-yellow-300 focus:bg-yellow-300 focus:ring focus:ring-yellow-300 focus:ring-offset-2"
+          onClick={handleUserName}
+        >
+          START ORDERING
+        </button>
+      )}
     </div>
   );
 };
--- src/redux/reducers/userSlice.js
+++ src/redux/reducers/userSlice.js
@@ -3,10 +3,17 @@ import { createSlice } from "@reduxjs/toolkit";
 const userSlice = createSlice({
   name: "user",
   initialState: {
-    name: "",
+    name: localStorage.getItem("username") || "",
     orders: {},
   },
-  reducers: {},
+  reducers: {
+    addUsername(state, action) {
+      state.name = action.payload;
+      localStorage.setItem("username", action.payload);
+    },
+  },
 });
 
+export const selectUserName = (state) => state.user.name;
+export const { addUsername } = userSlice.actions;
 export default userSlice.reducer;
