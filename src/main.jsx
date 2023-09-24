55901804c7eb4a0b35067ce340bdd24ed89506fe
dosepehr <dosepehr@gmail.com>
2023-09-24 15:45:42 +0330

    add tailwind css

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,7 @@ import { router } from './router/index.jsx';
 import { Provider } from 'react-redux';
 import { store } from './redux/store';
 
+import './index.css';
 createRoot(document.getElementById('root')).render(
     <StrictMode>
         <Provider store={store}>
