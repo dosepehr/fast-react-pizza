9ffb865ac2bedc71945dde89ba947f0cda2bd057
dosepehr <dosepehr@gmail.com>
2023-09-24 18:08:41 +0330

    add menu style

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,22 +1,20 @@
-import React from 'react';
-import { useGetMenuItemsQuery } from '../redux/reducers/apiSlice';
-import Loading from './Loading';
-import MenuItem from './MenuItem';
+import React from "react";
+import { useGetMenuItemsQuery } from "../redux/reducers/apiSlice";
+import Loading from "./Loading";
+import MenuItem from "./MenuItem";
 
 const Menu = () => {
-    const { data: menuItems, isLoading } = useGetMenuItemsQuery();
+  const { data: menuItems, isLoading } = useGetMenuItemsQuery();
 
-    return (
-        <div>
-            {!isLoading ? (
-                menuItems.data.map((pizza) => (
-                    <MenuItem pizza={pizza} key={pizza.id} />
-                ))
-            ) : (
-                <Loading />
-            )}
-        </div>
-    );
+  return (
+    <ul className="mx-auto mt-5 max-w-3xl divide-y divide-stone-200 px-1 sm:px-10">
+      {!isLoading ? (
+        menuItems.data.map((pizza) => <MenuItem pizza={pizza} key={pizza.id} />)
+      ) : (
+        <Loading />
+      )}
+    </ul>
+  );
 };
 
 export default Menu;
--- src/components/MenuItem.jsx
+++ src/components/MenuItem.jsx
@@ -1,17 +1,40 @@
-import React from 'react';
+import React from "react";
 
 const MenuItem = ({ pizza }) => {
-    const { id, name, unitPrice, ingredients, soldOut, imageUrl } = pizza;
-    return (
-        <li>
-            <img src={imageUrl} alt={name} />
-            <div>
-                <p>{name}</p>
-                <p>{ingredients.join(', ')}</p>
-                <div>{!soldOut ? <p>{unitPrice}</p> : <p>Sold out</p>}</div>
-            </div>
-        </li>
-    );
+  const { id, name, unitPrice, ingredients, soldOut, imageUrl } = pizza;
+  return (
+    <li className="flex gap-x-4 py-2">
+      <div>
+        <img
+          src={imageUrl}
+          alt={name}
+          className={`w-24 h-24 ${soldOut && "opacity-70 grayscale"}`}
+        />
+      </div>
+      <div className="flex grow flex-col">
+        <p className="font-medium">{name}</p>
+        <p className="text-sm capitalize italic text-stone-500">
+          {ingredients.join(", ")}
+        </p>
+        <div className="mt-auto flex w-full items-center justify-between">
+          <div>
+            {!soldOut ? (
+              <p className="text-sm">{unitPrice}</p>
+            ) : (
+              <p className="text-sm font-medium uppercase text-stone-500">
+                Sold out
+              </p>
+            )}
+          </div>
+          {!soldOut && (
+            <button className="rounded-full bg-yellow-400 px-4 py-2 text-xs font-semibold tracking-wide text-stone-800 outline-none duration-300 hover:bg-yellow-300 focus:bg-yellow-300 focus:ring focus:ring-yellow-300 focus:ring-offset-2 uppercase">
+              add to cart
+            </button>
+          )}
+        </div>
+      </div>
+    </li>
+  );
 };
 
 export default MenuItem;
